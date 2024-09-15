eeb0a11ffe62c865fe7037ca89d2af0709170fe7
RehaanAnjariaAgain <rehaananjariaagain1@MacBook-Air-4.local>
2024-09-15 17:44:14 -0400

    major changes

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-﻿
+﻿// Set nav underlinings:
+window.addEventListener("DOMContentLoaded", () => {
+    let activePage = window.location.pathname;
+    console.log(retrivePathName(activePage));
+    activePage = retrivePathName(activePage);
+    document.querySelectorAll(".navlink").forEach((link) => {
+        if (link.href === window.location.href) {
+            console.log(window.location.ref);
+            link.classList.add("isactive");
+        }
+    });
+});
+
+function retrivePathName(path) {
+    const longUrl = "/Users/rehaananjariaagain1/Library/Mobile%20Documents/com~apple~CloudDocs/Desktop/kinochronika_web/homedir/public_html/";
+    const length = longUrl.length;
+    return path.substring(length);
+}
